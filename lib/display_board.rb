8ae8128b5ff0c87ae443f3cfc46e233a98e15df5
Chris Kirchner <kirch2@gmail.com>
2021-06-22 19:03:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
 
-def display_board
+def display_board(board)
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
